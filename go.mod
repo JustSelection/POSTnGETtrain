89f9d2b96b2b2587b4bdab0c90803d8eecd537cb
JustSelection <grishan.psih@gmail.com>
2025-08-06 09:25:44 +0300

    Второй комит

--- go.mod
+++ go.mod
@@ -1,1 +1,23 @@
 module POSTnGETtrain
+
+go 1.24.5
+
+require (
+	github.com/ebitengine/gomobile v0.0.0-20240911145611-4856209ac325 // indirect
+	github.com/ebitengine/hideconsole v1.0.0 // indirect
+	github.com/ebitengine/purego v0.8.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/hajimehoshi/ebiten/v2 v2.8.8 // indirect
+	github.com/jezek/xgb v1.1.1 // indirect
+	github.com/labstack/echo/v4 v4.13.4 // indirect
+	github.com/labstack/gommon v0.4.2 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	golang.org/x/crypto v0.38.0 // indirect
+	golang.org/x/net v0.40.0 // indirect
+	golang.org/x/sync v0.14.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
+	golang.org/x/text v0.25.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,3 +1,60 @@
 package main
 
-func main() {}
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+var task string = "World!" //по умолчанию
+
+// answ - http ответ
+func handlerPOST(answ http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		http.Error(answ, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var requestBody struct {
+		Task string `json:"task"`
+	}
+
+	err := json.NewDecoder(req.Body).Decode(&requestBody)
+	if err != nil {
+		http.Error(answ, "Bad Request", http.StatusBadRequest)
+		return
+	}
+	task = requestBody.Task
+
+	//подготовка ответа
+	response := map[string]string{
+		"message": "Task изменен на: " + task, //"message" - ключ к http
+	}
+
+	//заворачивание ответа в json
+	answ.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(answ).Encode(response)
+}
+
+func handlerGET(answ http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodGet {
+		http.Error(answ, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	response := map[string]string{
+		"message": "hello, " + task,
+	}
+
+	answ.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(answ).Encode(response)
+}
+
+func main() {
+	http.HandleFunc("/", handlerGET)
+	http.HandleFunc("/task", handlerPOST)
+	log.Println("Сервер запущен на http://localhost:8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("Ошибка сервера: ", err)
+	}
+}
